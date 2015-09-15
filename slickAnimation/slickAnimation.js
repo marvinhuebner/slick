b213c761d17c32cdef4a81d5e7a6ddd1d87f3244
Marvin Hübner <mjh@bergwerk.ag>
2015-09-15 23:24:45 +0200

    [task] add delayIn, delayOut, animateIn, AnimateOut

--- slickAnimation/slickAnimation.js
+++ slickAnimation/slickAnimation.js
@@ -1,20 +1,44 @@
-
-(function( $ ){
-    $.fn.slickAnimation = function() {
+(function ($) {
+    $.fn.slickAnimation = function () {
         var currentSlickSlider = $(this);
 
         var slickItems = currentSlickSlider.find('.slick-list .slick-track > div');
 
-        slickItems.each(function(){
-           var slickItem = $(this);
+        slickItems.each(function () {
+            var slickItem = $(this);
 
-            slickItem.find('div[data-animation-in]').each(function(){
-               var dataAnimationIn = $(this);
+            slickItem.find('[data-animation-in]').each(function () {
+                var dataAnimationIn = $(this);
 
                 var animationIn = dataAnimationIn.attr('data-animation-in');
 
                 console.log(animationIn);
             });
+
+            slickItem.find('[data-animation-out]').each(function () {
+                var dataAnimationOut = $(this);
+
+                var animationOut = dataAnimationOut.attr('data-animation-out');
+
+                console.log(animationOut);
+            });
+
+            slickItem.find('[data-delay-in]').each(function () {
+                var dataDelayIn = $(this);
+
+                var delayIn = dataDelayIn.attr('data-delay-in');
+
+                console.log(delayIn);
+            });
+
+            slickItem.find('[data-delay-out]').each(function(){
+                var dataDelayOut = $(this);
+
+                var delayOut = dataDelayOut.attr('data-delay-out');
+
+                console.log(delayOut);
+            });
+
         });
     }
-})( jQuery );
+})(jQuery);
