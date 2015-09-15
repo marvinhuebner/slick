1c5150cea1fe32693320530ab32cdfa05f22c834
Marvin Hübner <mjh@bergwerk.ag>
2015-09-15 21:11:09 +0200

    [task] custom.js for sample init

---
+++ slickAnimation/custom.js
@@ -0,0 +1,12 @@
+var sliderInit = $('.slick-slider-init');
+
+$(document).ready(function(){
+
+    sliderInit.slick({
+        autoplay: false,
+        pauseOnHover: false,
+        speed: 1500,
+        autoplaySpeed: 6000
+    });
+
+});
